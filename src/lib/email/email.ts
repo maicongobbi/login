6f41b01cedaf2ab8409489698830e88a2d6997dc
maicon gobbi <maicongobbi>
2025-06-26 16:49:35 -0400

    resend config

--- src/lib/email/email.ts
+++ src/lib/email/email.ts
@@ -9,6 +9,7 @@ import { Resend } from 'resend';
 });
  */
 const resend = new Resend(process.env.RESEND_API_KEY);
+resend.apiKeys.create({ name: 'Production' });
 
 export async function sendEmail(email: string, subject: string, emailHtml: string) {
 
